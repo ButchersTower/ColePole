3502578916306f033a18993704d9d17bb4c05b87
ButchersTower <ButchersTwoer@gmail.com>
2014-10-19 03:45:06 -0400

    renaming

--- ColePole.java
+++ ColePole.java
@@ -6,7 +6,7 @@ public class ColePole extends JFrame {
 	public ColePole() {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(new Panel());
+		frame.setContentPane(new OneCirc());
 		frame.pack();
 		frame.setVisible(true);
 		frame.setResizable(false);
--- OneCirc.java
+++ OneCirc.java
@@ -13,7 +13,7 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
-public class PanelOld1 extends JPanel implements Runnable, MouseListener,
+public class OneCirc extends JPanel implements Runnable, MouseListener,
 		KeyListener {
 
 	int width = 540;
@@ -55,12 +55,15 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 	float[][] direction = new float[0][0];
 	int pathPart = 0;
 
-	boolean setPath = false;
-	float[] path = new float[0];
-	boolean moving = false;
 	int lowest = 0;
+	float playSpeedLeft;
+	float[] path = new float[0];
+	float[] myPath;
+	boolean setPath = false;
+	boolean moving = false;
+	boolean pathing = false;
 
-	public Panel() {
+	public OneCirc() {
 		super();
 
 		setPreferredSize(new Dimension(width, height));
@@ -128,6 +131,9 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 				draw();
 				drwGm();
 			}
+			if (pathing) {
+				followPath();
+			}
 			// System.out.println("runTime: " + timer());
 			// And above here.
 
@@ -176,37 +182,12 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 		}
 	}
 
-	void playerMoveSeg() {
-		// get delta vector. scale to moveSpeed.
-		float[] deltaVect = { tarX - pX, tarY - pY };
-		float deltaR = Vect2d.norm(deltaVect);
-		if (deltaR > pSpeed) {
-			float scalarMult = pSpeed / deltaR;
-			deltaVect = Vect2d.vectMultScalar(scalarMult, deltaVect);
-		}
-
-		if (distPointToVect(Vect2d.vectSub(new float[] { treeInfo[0],
-				treeInfo[1] }, new float[] { pX, pY }), deltaVect) < treeInfo[2]
-				+ pRadius) {
-			moving = false;
-			// cant move
-		} else {
-			pX += deltaVect[0];
-			pY += deltaVect[1];
-		}
-	}
-
 	void playMoveWhole() {
 		// Plus thea from player should get sub thea from tar.
 
 		direction = new float[0][];
 		// get delta vector. scale to moveSpeed.
 		float[] deltaVect = { tarX - pX, tarY - pY };
-		float deltaR = Vect2d.norm(deltaVect);
-		// if (deltaR > pSpeed) {
-		// float scalarMult = pSpeed / deltaR;
-		// deltaVect = Vect2d.vectMultScalar(scalarMult, deltaVect);
-		// }
 
 		if (distPointToVect(Vect2d.vectSub(new float[] { treeInfo[0],
 				treeInfo[1] }, new float[] { pX, pY }), deltaVect) < treeInfo[2]
@@ -231,14 +212,6 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 			tangents = myAngleThing(new float[] { tarX, tarY }, pRadius,
 					new float[] { treeInfo[0], treeInfo[1] }, treeInfo[2]);
 
-			// float[][] toTar = new float[0][];
-			// toTar = JaMa.appendFloatArAr(direction, new float[] {
-			// tangents[0],
-			// tangents[1], tangents[6] });
-			// toTar = JaMa.appendFloatArAr(direction, new float[] {
-			// tangents[3],
-			// tangents[4], tangents[6] });
-
 			// tangents from tar
 			// plusThea from player should get subThea from tar.
 			direction[0] = JaMa.appendArFloatAr(direction[0], new float[] {
@@ -247,16 +220,24 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 					tangents[2], pRadius + treeInfo[2] });
 
 			direction[0] = JaMa.appendArFloatAr(direction[0], new float[] { 0,
-					tarX, tarY, tangents[6] });
+					-tangents[3], -tangents[4], tangents[6] });
 			direction[1] = JaMa.appendArFloatAr(direction[1], new float[] { 0,
-					tarX, tarY, tangents[6] });
+					-tangents[0], -tangents[1], tangents[6] });
+			System.out.println("direction[0][" + 9 + "]: " + direction[0][9]);
+			System.out.println("direction[0][" + 10 + "]: " + direction[0][10]);
+			System.out.println("direction[1][" + 9 + "]: " + direction[1][9]);
+			System.out.println("direction[1][" + 10 + "]: " + direction[1][10]);
+
 			System.out.println("makePath");
 			sortDirections();
-
+			for (int d = 0; d < direction[lowest].length; d++) {
+				System.out.println("direction[" + d + "]: "
+						+ direction[lowest][d]);
+			}
+			myPath = direction[lowest];
+			pathing = true;
 		} else {
 			path = deltaVect;
-			// pX += deltaVect[0];
-			// pY += deltaVect[1];
 		}
 	}
 
@@ -267,23 +248,8 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 		for (int d = 0; d < direction.length; d++) {
 			for (int i = 0; i < direction[d].length / 4; i++) {
 				if (direction[d][i * 4] == 0) {
-					// System.out.println("direction[" + d + "][" + (i * 4 + 3)
-					// + "]: " + direction[d][i * 4 + 3]);
 					sums[d] += direction[d][i * 4 + 3];
 				} else {
-					// System.out.println("direction["+d+"][" + (i * 4 + 1) +
-					// "]"
-					// + direction[d][i * 4 + 1]);
-					// System.out.println("direction["+d+"][" + (i * 4 + 2) +
-					// "]"
-					// + direction[d][i * 4 + 2]);
-					// System.out.println("direction["+d+"][" + (i * 4 + 3) +
-					// "]"
-					// + direction[d][i * 4 + 3]);
-					// System.out
-					// .println("Get dist of edge: "
-					// + (JaMa.theaSub(direction[d][i * 4 + 1],
-					// direction[d][i * 4 + 2]) * direction[d][i * 4 + 3]));
 					sums[d] += Math.abs(JaMa.theaSub(direction[d][i * 4 + 1],
 							direction[d][i * 4 + 2]) * direction[d][i * 4 + 3]);
 				}
@@ -291,62 +257,127 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 		}
 		// find the lowest sum and follow that.
 		lowest = 0;
-		System.out.println("sums[" + 0 + "]: " + sums[0]);
+		// System.out.println("sums[" + 0 + "]: " + sums[0]);
 		for (int s = 1; s < sums.length; s++) {
-			System.out.println("sums[" + s + "]: " + sums[s]);
+			// System.out.println("sums[" + s + "]: " + sums[s]);
 			if (sums[s] < sums[lowest]) {
 				lowest = s;
 			}
 		}
-		System.out.println("lowest: " + lowest);
+		// System.out.println("lowest: " + lowest);
+	}
 
-		// g.setColor(lowest == 0 ? Color.RED : Color.ORANGE);
-		// g.drawLine((int) pX, (int) pY, (int) (pX + direction[0][1]),
-		// (int) (pY + direction[0][2]));
-		// g.setColor(lowest == 1 ? Color.RED : Color.ORANGE);
-		// g.drawLine((int) pX, (int) pY, (int) (pX + direction[1][1]),
-		// (int) (pY + direction[1][2]));
-		// drwGm();
+	void followPathSwitchBreak() {
+		// uses switch
+		// Assumes that
+		// if [0] == 0 then line.
+		// if [0] == 1 then curve.
+		float[] myPath = direction[lowest];
+		float playSpeedLeft = pSpeed;
+		// if (myPath[0] == 0) {
+		System.out.println("(int) myPath[0]: " + ((int) myPath[0]));
+		switch ((int) myPath[0]) {
+		case 0:
+			System.out.println("case 0");
+			// System.out.println("line");
+			// linear so go straight
+			if (myPath[3] > playSpeedLeft) {
+				System.out.println("playSpeedLeft : " + playSpeedLeft);
+				System.out.println("myPath[3]: " + myPath[3]);
+				// Vect2d.sayVect("myPath", myPath);
+				pX += myPath[1] * playSpeedLeft;
+				pY += myPath[2] * playSpeedLeft;
+				myPath[3] -= playSpeedLeft;
+			} else {
 
-		// now follow the path
-		// moving = false;
+				pX += myPath[1] * myPath[3];
+				pY += myPath[2] * myPath[3];
+				playSpeedLeft -= myPath[3];
+				// delete the four. and move on.
+				System.out.println("myPath.l: " + myPath.length);
+				myPath = JaMa.removeFirstFloatAr(myPath, 4);
+				System.out.println("myPath.l: " + myPath.length);
+			}
+			// }
+			// if (myPath[0] == 1) {
+			break;
+		case 1:
+			System.out.println("case 1");
+			// around edge
+			float edgeLength = JaMa.theaSub(myPath[1], myPath[2]) * myPath[3];
+			break;
+		}
 	}
 
 	void followPath() {
-		float[] myPath = direction[lowest];
-		for (int r = 0; r < myPath.length / 4; r++) {
-			if (myPath[r * 4] == 0) {
-				// linear so go straight
-			} else if (myPath[r * 4] == 1) {
-				// around edge
-			}
+		playSpeedLeft = pSpeed;
+		while (myPath.length > 0 && playSpeedLeft > 0) {
+			System.out.println("while");
+			sortPath();
 		}
 	}
 
-	float distPointToSeg(float[] point, float[][] seg) {
-		// project, is projection scalar is farther than the line then take
-		// hypotnuse of closest and edge and point. if the scalar is on the line
-		// then reject and that is dist.
-
-		float[] pointRelSeg = Vect2d.vectSub(point, seg[0]);
-		float[] segRelSeg = Vect2d.vectSub(seg[1], seg[0]);
-		//
-		float dist;
-		float projScalar = Vect2d.scalarOfProject(pointRelSeg, segRelSeg);
-		if (projScalar > 1) {
-			// get dist from the end of seg.
-			dist = Vect2d.norm(new float[] { seg[1][0] - point[0],
-					seg[1][1] - point[1] });
-		} else if (projScalar < 0) {
-			// get the dist from the start of seg.
-			dist = Vect2d.norm(new float[] { seg[0][0] - point[0],
-					seg[0][1] - point[1] });
-		} else {
-			// dist is point rej proj
-			dist = Vect2d.norm(Vect2d.vectSub(point,
-					Vect2d.vectMultScalar(projScalar, segRelSeg)));
+	void sortPath() {
+		if (myPath[0] == 0) {
+			System.out.println("path 0");
+			// System.out.println("line");
+			// linear so go straight
+			if (myPath[3] > playSpeedLeft) {
+				System.out.println("playSpeedLeft : " + playSpeedLeft);
+				System.out.println("myPath[3]: " + myPath[3]);
+				// Vect2d.sayVect("myPath", myPath);
+				pX += myPath[1] * playSpeedLeft;
+				pY += myPath[2] * playSpeedLeft;
+				// System.out.println("myPath[1]: " + myPath[1]);
+				// System.out.println("myPath[2]: " + myPath[2]);
+				// System.out.println("xAdd: " + myPath[1] * playSpeedLeft);
+				// System.out.println("yAdd: " + myPath[2] * playSpeedLeft);
+				myPath[3] -= playSpeedLeft;
+				playSpeedLeft = 0;
+			} else {
+				pX += myPath[1] * myPath[3];
+				pY += myPath[2] * myPath[3];
+				playSpeedLeft -= myPath[3];
+				// delete the four. and move on.
+				myPath = JaMa.removeFirstFloatAr(myPath, 4);
+			}
+		} else if (myPath[0] == 1) {
+			System.out.println("path 1");
+			// around edge
+			float edgeLength = Math.abs(JaMa.theaSub(myPath[1], myPath[2])
+					* myPath[3]);
+			System.out.println("edgeLength: " + edgeLength);
+			if (edgeLength > playSpeedLeft) {
+				// figure out if plus thea is closer or minus thea, then move
+				// accordingly inorder to fuffil moveSpeedLeft.
+				float possableThea = playSpeedLeft / myPath[3];
+				System.out.println("possableThea: " + possableThea);
+				float newThea;
+				if (lowest == 0) {
+					newThea = myPath[2] + possableThea;
+					// add thea
+				} else {
+					newThea = myPath[2] - possableThea;
+					// sub thea
+				}
+				float[] newLoc = JaMa.theaToPoint(newThea, myPath[3]);
+				myPath[2] = newThea;
+				g.setColor(Color.MAGENTA);
+				g.drawOval((int) (treeInfo[0] + newLoc[0]) - 3,
+						(int) (treeInfo[1] + newLoc[1]) - 3, 6, 6);
+				pX = treeInfo[0] + newLoc[0];
+				pY = treeInfo[1] + newLoc[1];
+				System.out.println("pX: " + pX + ",   pY: " + pY);
+				// pathing = false;
+				// playSpeedLeft = 0;
+			} else {
+				float[] newLoc = JaMa.theaToPoint(myPath[2], myPath[3]);
+				pX = treeInfo[0] + newLoc[0];
+				pY = treeInfo[1] + newLoc[1];
+				myPath = JaMa.removeFirstFloatAr(myPath, 4);
+				playSpeedLeft -= edgeLength;
+			}
 		}
-		return dist;
 	}
 
 	float distPointToVect(float[] point, float[] vect) {
@@ -356,7 +387,7 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 
 		float dist;
 		float projScalar = Vect2d.scalarOfProject(point, vect);
-		System.out.println("projScalar: " + projScalar);
+		// System.out.println("projScalar: " + projScalar);
 		if (projScalar > 1) {
 			// get dist from the end of seg.
 			dist = Vect2d.norm(Vect2d.vectSub(point, vect));
@@ -369,12 +400,6 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 					Vect2d.vectMultScalar(projScalar, vect)));
 		}
 		return dist;
-	}
-
-	void playGoTo() {
-		// player go to targetLoc and.
-		// proj a vector of the length of
-		// get vecto
 	}
 
 	void draw() {
@@ -390,6 +415,7 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 				(int) treeInfo[2] * 2);
 	}
 
+	// [1, 2, 3] sometimes is NaN
 	float[] myAngleThing(float[] play, float pRad, float[] tree, float tRad) {
 		// if |delta| < pRad + tRad
 		// get thea of play and project it out from tree to a dist of pRad+tRad.
@@ -397,10 +423,12 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 		float[] delta = Vect2d.vectSub(tree, play);
 		float hyp = Vect2d.norm(delta);
 		float opp = pRad + tRad;
-		float adj = (float) Math.sqrt(hyp * hyp - opp * opp);
-		// float treeThea = (float) Math.atan(delta[1] / delta[0]);
+		System.out.println("hyp * hyp: " + hyp * hyp);
+		System.out.println("opp * opp: " + opp * opp);
+		float adj = (float) Math.sqrt(Math.abs(hyp * hyp - opp * opp));
+		System.out.println("adj: " + adj);
+		System.out.println("opp: " + opp);
 		float treeThea = JaMa.pointToThea(delta);
-		// float shapeThea = (float) Math.asin(opp / hyp);
 		float shapeThea = JaMa.pointToThea(new float[] { adj, opp });
 		// how to tell is to subtract or add shape thea.
 		// the two possible points are plus shape thea and minus shape thea
@@ -409,35 +437,37 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 		// [0 + 1] is (x, y) of plus thea
 		// [2 + 3] is (x, y) of minus thea
 		// [4] is the length from play to each point.
+		System.out.println("treeThea: " + treeThea);
+		System.out.println("shapeThea: " + shapeThea);
 		float addThea = JaMa.theaAdd(treeThea, shapeThea);
+		System.out.println("addThea: " + addThea);
 		float subThea = JaMa.theaSub(treeThea, shapeThea);
-		// System.out.println("plusThea: " + addThea + "   : subThea: " +
-		// subThea);
-		float[] plusPoint = JaMa.theaToPoint(addThea, adj);
+		float[] addPoint = JaMa.theaToPoint(addThea, adj);
 		float[] subPoint = JaMa.theaToPoint(subThea, adj);
 		// make sub thea and plus thea relative to tree.
 		// plus point minus tree
-		float[] relAddPoint = Vect2d.vectSub(Vect2d.vectAdd(play, plusPoint),
+		float[] relAddPoint = Vect2d.vectSub(Vect2d.vectAdd(play, addPoint),
 				tree);
+		Vect2d.sayVect("play", play);
+		Vect2d.sayVect("addPoint", addPoint);
+		Vect2d.sayVect("relAddPoint", relAddPoint);
 		float[] relSubPoint = Vect2d.vectSub(Vect2d.vectAdd(play, subPoint),
 				tree);
-		// Vect2d.sayVect("relPlusPoint", relAddPoint);
-		// Vect2d.sayVect("relSubPoint", relSubPoint);
 		g.setColor(Color.GREEN);
 		g.drawOval((int) (relAddPoint[0] + tree[0]) - 4,
 				(int) (relAddPoint[1] + tree[1]) - 4, 8, 8);
 		g.setColor(Color.CYAN);
 		g.drawOval((int) (relSubPoint[0] + tree[0]) - 4,
 				(int) (relSubPoint[1] + tree[1]) - 4, 8, 8);
-		float relPlusThea = JaMa.pointToThea(relAddPoint);
+		float relAddThea = JaMa.pointToThea(relAddPoint);
+		System.out.println("relAddThea: " + relAddThea);
 		float relSubThea = JaMa.pointToThea(relSubPoint);
-		// System.out.println("relPlusThea: " + relPlusThea +
-		// "  :  relSubThea: "
-		// + relSubThea);
-		// Vect2d.sayVect("plusPoint", plusPoint);
-		// Vect2d.sayVect("subPoint", subPoint);
-		return new float[] { plusPoint[0], plusPoint[1], relPlusThea,
-				subPoint[0], subPoint[1], relSubThea, adj };
+		addPoint = Vect2d.normalize(addPoint);
+		Vect2d.sayVect("addPoint", addPoint);
+		subPoint = Vect2d.normalize(subPoint);
+		System.out.println("subPoint[0]: " + subPoint[0]);
+		return new float[] { addPoint[0], addPoint[1], relAddThea, subPoint[0],
+				subPoint[1], relSubThea, adj };
 	}
 
 	/**
@@ -467,6 +497,7 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 			// moving = false;
 			setPath = false;
 		}
+		pathing = false;
 	}
 
 	@Override
@@ -494,22 +525,25 @@ public class PanelOld1 extends JPanel implements Runnable, MouseListener,
 	}
 
 	@Override
+	public void keyPressed(KeyEvent ke) {
+		if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, width, height);
+			moving = false;
+			pathing = false;
+		}
+		draw();
+		drwGm();
+	}
+
+	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void keyPressed(KeyEvent ke) {
-		if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
-			g.setColor(Color.BLACK);
-			g.fillRect(0, 0, width, height);
-		}
-	}
-
-	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 }
